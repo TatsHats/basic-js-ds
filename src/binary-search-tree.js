0e887e8fffda7458273a892bef36643bcc70f36d
TatsHats <6642017@gmail.com>
2025-01-05 21:10:52 +0200

    feat: 7 passing (Вс, янв 05, 2025 09:10:52 )

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,26 +6,69 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
 
+  constructor() {
+    this.rootTree = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootTree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    // создать узел (node), где данные это data.
+    const newNode = new Node(data);
+    
+    // если дерево пустое, то сделать новый узел корнем
+    if (this.rootTree === null) {
+      this.rootTree = newNode;
+    } else {
+      // вставить узел рекурсивно (вспомог. функция addNode вставляет в нужное место, спускаясь по дереву, сравнивая элементы)
+      this.addNode(this.rootTree, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  //вспом. функция рекурсивного поиска
+  addNode(node, newNode) {
+    const direction = newNode.data < node.data ? 'left' : 'right'; // перем. хранит направление
+
+    if (node[direction] === null) {   // если знач. пусто вставл. в нужное направление
+      node[direction] = newNode;
+    } else {                          // иначе ищем дальше место для нового узла
+      this.addNode(node[direction], newNode);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.search(this.rootTree, data) !== null;
+  }
+
+  // вспом. ф. найти значение
+  search (node, data) {
+    const direction = data < node.data ? 'left' : 'right';
+
+    if (node === null) {
+      return null; // если узла нет вернем null
+    }
+    
+    if (node.data === data) {
+      return node; // если узел найден вернем его
+    }
+
+    return this.search(node[direction], data); // если не нашли ищем рекурсивно дальше в нужном направлении
+  }
+
+  find(data) {
+    return this.search(this.rootTree, data);
   }
 
   remove(/* data */) {
